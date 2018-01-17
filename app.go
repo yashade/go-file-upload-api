2ba8a60da64bc53cc066225a3e571026c286ed12
Yashar Ahmedizade <yashade2001@gmail.com>
2018-01-17 16:52:42 +0300

    auth

--- app.go
+++ app.go
@@ -20,24 +20,36 @@ func main() {
 	viper.SetDefault("fcgi", false)
 	viper.SetDefault("port", "9999")
 	viper.SetDefault("perms", 0666)
+	viper.SetDefault("key", "mykey")
 
 	router := httprouter.New()
 
 	router.POST("/upload", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		file, handler, err := r.FormFile("file")
-		defer file.Close()
-		if err != nil {
-			log.Fatal(err)
+		key := r.Header.Get("Key")
+
+		if key == viper.GetString("key") {
+			file, handler, err := r.FormFile("file")
+			defer file.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			name := handler.Filename + "---" + time.Now().UTC().Format("2006-01-02_15:04:05")
+
+			f, err := os.OpenFile(
+				name,
+				os.O_WRONLY|os.O_CREATE,
+				os.FileMode(viper.GetInt("perms")))
+			defer f.Close()
+
+			log.Printf(handler.Filename)
+			io.Copy(f, file)
+
+			w.Write([]byte("\n" + name + "\n\n"))
+		} else {
+			w.WriteHeader(401)
+			w.Write([]byte("\n" + "Unauthorized" + "\n\n"))
 		}
-
-		f, err := os.OpenFile(
-			handler.Filename+"---"+time.Now().UTC().Format("2006-01-02_15:04:05"),
-			os.O_WRONLY|os.O_CREATE,
-			os.FileMode(viper.GetInt("perms")))
-		defer f.Close()
-
-		log.Printf(handler.Filename)
-		io.Copy(f, file)
 	})
 
 	if viper.GetBool("fcgi") {
